7252545f336b7a82ac147f63968b41f16ff59421
Tiffecko <113545856+Tiffecko@users.noreply.github.com>
2022-12-21 18:38:28 +0500

    Add files via upload

--- LabNum2/Program.cs
+++ LabNum2/Program.cs
@@ -1,82 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Labs
+
+public class LabNum2
 {
-    public class LabNum2
+    public static void PrintProcesedInput(string userInput, bool userNumbIsNegative)
     {
-        public static void PrintProcesedInput(string userInput)
+        userInput = Console.ReadLine();
+
+        if ((userInput == string.Empty) || (!IsStrNumberCorrect(userInput, out userNumbIsNegative)))
         {
-            string userInput = Console.ReadLine();
-            bool userNumbIsNegative;
-            if ((userInput == string.Empty) || (!IsStrNumberCorrect(userInput, out userNumbIsNegative)))
-            {
-                Console.WriteLine($"Некорректный ввод");
-                return;
-            }
-            else
-            {
-                int indexator = 0;
-                int sum = 0;
-
-                if (userNumbIsNegative)
-                {
-                    sum -= userInput[1] - '0';
-                    indexator += 2;
-                }
-
-                for (; indexator < userInput.Length; indexator++)
-                    sum += userInput[indexator] - '0';
-
-                Console.WriteLine(sum);
-            }
+            Console.WriteLine("Некорректный ввод!");
+            return;
         }
-        private static bool IsStrNumberCorrect(string strNumber, out bool isNegativeNumber)
+        else
         {
-            int positionIndex = 0;
+            int indexator = 0;
+            int sum = 0;
 
-            isNegativeNumber = false;
-
-            if (strNumber[0] == '-' && strNumber.Length > 1)
+            if (userNumbIsNegative)
             {
-                isNegativeNumber = true;
-
-                if (!char.IsDigit(strNumber[0 + 1]))
-                    return false;
-
-                positionIndex += 2;
+                sum -= userInput[1] - '0';
+                indexator += 2;
             }
 
-            for (; positionIndex < strNumber.Length; positionIndex++)
-                if (!char.IsDigit(strNumber[positionIndex]))
-                    return false;
+            for (; indexator < userInput.Length; indexator++)
+                sum += userInput[indexator] - '0';
 
-            return IsSrtNumberNotOverflow(strNumber, isNegativeNumber);
-        }
-        private static bool IsSrtNumberNotOverflow(string strNumber, bool numberIsNegative)
-        {
-            string _tempMinInt;
-
-            if (numberIsNegative)
-                _tempMinInt = int.MinValue.ToString();
-            else
-                _tempMinInt = int.MaxValue.ToString();
-
-            if (_tempMinInt.Length == strNumber.Length)
-            {
-                for (int i = 1; i < strNumber.Length; i++)
-                    if (strNumber[i] != _tempMinInt[i])
-                        return strNumber[i] < _tempMinInt[i];
-            }
-            else
-            {
-                return _tempMinInt.Length > strNumber.Length;
-            }
-
-            return true;
+            Console.WriteLine(sum);
         }
     }
+    private static bool IsStrNumberCorrect(string strNumber, out bool isNegativeNumber)
+    {
+        int positionIndex = 0;
+
+        isNegativeNumber = false;
+
+        if (strNumber[0] == '-' && strNumber.Length > 1)
+        {
+            isNegativeNumber = true;
+
+            if (!char.IsDigit(strNumber[0 + 1]))
+                return false;
+
+            positionIndex += 2;
+        }
+
+        for (; positionIndex < strNumber.Length; positionIndex++)
+            if (!char.IsDigit(strNumber[positionIndex]))
+                return false;
+
+        return IsSrtNumberNotOverflow(strNumber, isNegativeNumber);
+    }
+    private static bool IsSrtNumberNotOverflow(string strNumber, bool numberIsNegative)
+    {
+        string _tempMinInt;
+
+        if (numberIsNegative)
+            _tempMinInt = int.MinValue.ToString();
+        else
+            _tempMinInt = int.MaxValue.ToString();
+
+        if (_tempMinInt.Length == strNumber.Length)
+        {
+            for (int i = 1; i < strNumber.Length; i++)
+                if (strNumber[i] != _tempMinInt[i])
+                    return strNumber[i] < _tempMinInt[i];
+        }
+        else
+        {
+            return _tempMinInt.Length > strNumber.Length;
+        }
+
+        return true;
+    }
 }
+
