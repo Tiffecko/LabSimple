5fda0ea07fa2f978a57168384bd3ba653a2e9d4b
Tiffecko <113545856+Tiffecko@users.noreply.github.com>
2022-12-29 20:11:05 +0500

    Add files via upload

---
+++ LabNum8_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> array = new List<string> { "code", "doce", "ecod", "framer", "frame", "frame" };
+        AnagramBuild(array);
+    }
+
+    public static bool AnagramСomparison(string one, string two)
+    {
+        char[] char1 = one.ToCharArray();
+        char[] char2 = two.ToCharArray();
+
+        Array.Sort(char1);
+        Array.Sort(char2);
+        return (char1 == char2);
+    }
+
+    static void AnagramBuild(List<string> reultArray)
+    {
+        reultArray.Sort();
+        for (int i = 0; i < reultArray.Count; i++)
+        {
+            if (AnagramСomparison(reultArray[i], reultArray[i + 1]))
+                reultArray[i + 1] = " ";
+            if (reultArray[i] == " ")
+                reultArray.Remove(reultArray[i]);
+        }
+    }
+}
